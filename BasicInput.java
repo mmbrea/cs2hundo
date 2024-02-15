6136e621c1b1d689bda9edfa489de19f6899b752
Amber Dahlberg <mmbrea@hotmail.com>
2024-02-15 17:20:46 -0600

    Zybooksch 3 start.

---
+++ BasicInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class BasicInput {
+  /**
+   * Program entry.
+   */
+  public static void main(String[] args) {
+    /*
+     * Program entry.
+     */
+  }
+}
