41a17ad825862fe3782298369152c1f1167bb3be
Amber Dahlberg <mmbrea@hotmail.com>
2024-02-29 10:59:11 -0600

    cleaning up files.

---
+++ cs200/humanGradedHomework/H6CustomApp.java
@@ -0,0 +1,36 @@
+///////////////////////// TOP OF FILE COMMENT BLOCK ////////////////////////////
+//
+// Title:           descriptive title of the program making use of this file
+// Course:          course number, term, and year
+//
+// Author:          your name
+// Email:           your @wisc.edu email address
+// Lecturer's Name: name of your lecturer
+//
+///////////////////////////////// CITATIONS ////////////////////////////////////
+//
+// Source or Recipient; Description
+// Examples (REMOVE in your code - unless Jane Doe helped you and you helped John Doe accordingly):
+// Jane Doe; helped me with for loop in reverse method
+// https://docs.oracle.com/javase/tutorial/java/nutsandbolts/for.html;
+//         counting for loop
+// John Doe; I helped with switch statement in main method.
+//
+///////////////////////////////// REFLECTION ///////////////////////////////////
+//
+// 1. Describe the problem you wrote the program to solve: TODO
+// 2. What are the biggest challenges you encountered: TODO
+// 3. What did you learn from this assignment: TODO
+//
+/////////////////////////////// 80 COLUMNS WIDE ////////////////////////////////
+
+import java.util.Scanner;
+
+public class H6CustomApp {
+
+    public static void main(String [] args) {
+        Scanner input = new Scanner(System.in);
+
+        input.close();
+    }
+}
